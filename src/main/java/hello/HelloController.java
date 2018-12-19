29d2f6901dabdb5597dedcaaa092b733811b5a5a
zvvr <zvvr@abc.com>
2018-12-19 11:45:12 +0800

    update api h get hostname

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,14 +1,24 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
+
+    @Value("${hostname}")
+    private String hostname;
     
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
     }
+
+    @RequestMapping("/h")
+    public String host() {
+        return "Greetings from Spring Boot!"+hostname;
+    }
+
     
 }
